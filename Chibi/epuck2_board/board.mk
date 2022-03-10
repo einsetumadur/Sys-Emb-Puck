1288b06ef2dcd44d97b9784ce712befbf03d407c
Gilles Regamey <gilles.regamey@epfl.ch>
2022-03-10 08:19:48 +0100

    ChibiOS added

---
+++ Chibi/epuck2_board/board.mk
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+# List of all the board related files.
+BOARDSRC = ./epuck2_board/board.c
+
+# Required include directories
+BOARDINC = ./epuck2_board/
+=======
+# List of all the board related files.
+BOARDSRC = $(CHIBIOS_EXT)/os/hal/boards/epuck2/board.c
+
+# Required include directories
+BOARDINC = $(CHIBIOS_EXT)/os/hal/boards/epuck2
+>>>>>>> TP3_working
